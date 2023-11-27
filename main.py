cfa0a4d90b21fd583fbe3843bf292fbe01ed904a
phaustin0 <zechy14@icloud.com>
2023-11-27 19:06:24 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Game:
+	def __init__(self):
+		pass
+
+	def new(self):
+		pass
+
+	def events(self):
+		pass
+
+	def update(self):
+		pass
+
+	def draw(self):
+		pass
+
+
+if __name__ == "__main__":
+	g = Game()
