62ad1ce1ac6360db8d1d9c42a55c2cbdbba4ee7b
phaustin0 <zechy14@icloud.com>
2023-11-29 01:29:37 +0530

    load pieces from fen string

--- main.py
+++ main.py
@@ -8,27 +8,29 @@ from pieces.rook import *
 from pieces.queen import *
 from pieces.king import *
 
-start_white = [
-    [black_rook, black_knight, black_bishop, black_queen, black_king, black_bishop, black_knight, black_rook],
-    [black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn],
-    [empty, empty, empty, empty, empty, empty, empty, empty,],
-    [empty, empty, empty, empty, empty, empty, empty, empty,],
-    [empty, empty, empty, empty, empty, empty, empty, empty,],
-    [empty, empty, empty, empty, empty, empty, empty, empty,],
-    [white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn,],
-    [white_rook, white_knight, white_bishop, white_queen, white_king, white_bishop, white_knight, white_rook],
-]
+# start_white = [
+#     [black_rook, black_knight, black_bishop, black_queen, black_king, black_bishop, black_knight, black_rook],
+#     [black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn],
+#     [empty, empty, empty, empty, empty, empty, empty, empty,],
+#     [empty, empty, empty, empty, empty, empty, empty, empty,],
+#     [empty, empty, empty, empty, empty, empty, empty, empty,],
+#     [empty, empty, empty, empty, empty, empty, empty, empty,],
+#     [white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn,],
+#     [white_rook, white_knight, white_bishop, white_queen, white_king, white_bishop, white_knight, white_rook],
+# ]
+start_white_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 
-start_black = [
-    [white_rook, white_knight, white_bishop, white_king, white_queen, white_bishop, white_knight, white_rook],
-    [white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn,],
-    [empty, empty, empty, empty, empty, empty, empty, empty,],
-    [empty, empty, empty, empty, empty, empty, empty, empty,],
-    [empty, empty, empty, empty, empty, empty, empty, empty,],
-    [empty, empty, empty, empty, empty, empty, empty, empty,],
-    [black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn],
-    [black_rook, black_knight, black_bishop, black_king, black_queen, black_bishop, black_knight, black_rook],
-]
+# start_black = [
+#     [white_rook, white_knight, white_bishop, white_king, white_queen, white_bishop, white_knight, white_rook],
+#     [white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn,],
+#     [empty, empty, empty, empty, empty, empty, empty, empty,],
+#     [empty, empty, empty, empty, empty, empty, empty, empty,],
+#     [empty, empty, empty, empty, empty, empty, empty, empty,],
+#     [empty, empty, empty, empty, empty, empty, empty, empty,],
+#     [black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn],
+#     [black_rook, black_knight, black_bishop, black_king, black_queen, black_bishop, black_knight, black_rook],
+# ]
+start_black_fen = "RNBKQBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbkqbnr"
 
 class Game:
 	def __init__(self):
@@ -48,22 +50,12 @@ class Game:
 
 	def new(self):
 		self.all_sprites = pygame.sprite.LayeredUpdates()
-		self.pieces = pygame.sprite.LayeredUpdates()
+		self.pieces_sprites = pygame.sprite.LayeredUpdates()
 		self.board = Board(self)
-		self.start = start_white if self.is_white else start_black
+		# self.start = start_white if self.is_white else start_black
+		self.start_fen = start_white_fen if self.is_white else start_black_fen
 
-		Pawn(self, self._get_positions()[0][0], self.is_white)
-		Pawn(self, self._get_positions()[0][1], not self.is_white)
-		Bishop(self, self._get_positions()[0][2], self.is_white)
-		Bishop(self, self._get_positions()[0][3], not self.is_white)
-		Knight(self, self._get_positions()[0][4], self.is_white)
-		Knight(self, self._get_positions()[0][5], not self.is_white)
-		Rook(self, self._get_positions()[0][6], self.is_white)
-		Rook(self, self._get_positions()[0][7], not self.is_white)
-		Queen(self, self._get_positions()[1][2], self.is_white)
-		Queen(self, self._get_positions()[1][3], not self.is_white)
-		King(self, self._get_positions()[1][4], self.is_white)
-		King(self, self._get_positions()[1][5], not self.is_white)
+		self.pieces = self._load_pieces_from_fen(self.start_fen)
 
 	def events(self):
 		for event in pygame.event.get():
@@ -88,6 +80,65 @@ class Game:
 				row.append((w*width/8 + self.start_x + width/16, h*height/8 + self.start_y + height/16))
 			pos.append(row)
 		return pos
+
+	# def _load_pieces(self):
+	# 	pieces = []
+	# 	for i in range(len(self.start)):
+	# 		for j in range(len(self.start[i])):
+	# 			piece = self.start[i][j]
+	# 			if piece == empty:
+	# 				continue
+
+	# 			is_white = piece[0] == "w"
+	# 			if piece[1] == "p":
+	# 				pieces.append(Pawn(self, self.positions[i][j], is_white))
+	# 			elif piece[1] == "k":
+	# 				pieces.append(Knight(self, self.positions[i][j], is_white))
+	# 			elif piece[1] == "b":
+	# 				pieces.append(Bishop(self, self.positions[i][j], is_white))
+	# 			elif piece[1] == "r":
+	# 				pieces.append(Rook(self, self.positions[i][j], is_white))
+	# 			elif piece[1] == "q":
+	# 				pieces.append(Queen(self, self.positions[i][j], is_white))
+	# 			elif piece[1] == "K":
+	# 				pieces.append(King(self, self.positions[i][j], is_white))
+	# 	return pieces
+
+	def _load_pieces_from_fen(self, fen):
+		pieces = []
+		rows = fen.split("/")
+		for i in range(len(rows)):
+			j = 0
+			row = []
+			for c in range(len(rows[i])):
+				if j >= 8: break
+
+				char = rows[i][c]
+				if str.isnumeric(char):
+					char = int(char)
+					empty = ["" for i in range(char)]
+					row = row + empty
+					j += char
+					continue
+
+				is_white = str.isupper(char)
+				if str.lower(char) == "p":
+					row.append(Pawn(self, self.positions[i][j], is_white))
+				elif str.lower(char) == "n":
+					row.append(Knight(self, self.positions[i][j], is_white))
+				elif str.lower(char) == "b":
+					row.append(Bishop(self, self.positions[i][j], is_white))
+				elif str.lower(char) == "r":
+					row.append(Rook(self, self.positions[i][j], is_white))
+				elif str.lower(char) == "q":
+					row.append(Queen(self, self.positions[i][j], is_white))
+				elif str.lower(char) == "k":
+					row.append(King(self, self.positions[i][j], is_white))
+
+				j += 1
+			pieces.append(row)
+
+		return pieces
 
 
 if __name__ == "__main__":
--- pieces/piece.py
+++ pieces/piece.py
@@ -6,20 +6,13 @@ from settings import *
 class Piece(pygame.sprite.Sprite):
     def __init__(self, game, pos, img_path):
         self.game = game
-        self.groups = self.game.all_sprites, self.game.pieces
+        self.groups = self.game.all_sprites, self.game.pieces_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
 
         self.is_moved = False
 
-        # scale = 2
         path = os.path.join(os.getcwd(),f"pieces/img/{img_path}.png")
-        # svg_string = open(path, "rt").read()
-        # start = svg_string.find('<svg')
-        # if start > 0:
-        #     svg_string = svg_string[:start+4] + f' transform="scale({scale},{scale})"' + svg_string[start+4:]
-        # self.image = pygame.image.load(io.BytesIO(svg_string.encode()))
         self.image = pygame.image.load(path)
-        # self.image = pygame.transform.smoothscale(self.image,(75,75))
         self.rect = self.image.get_rect()
         self.rect.center = pos
 
