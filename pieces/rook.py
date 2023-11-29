76661867f13bdeb77b6793f920aedf1612a68e1f
phaustin0 <zechy14@icloud.com>
2023-11-29 13:49:40 +0530

    added rook movement

--- pieces/rook.py
+++ pieces/rook.py
@@ -6,8 +6,48 @@ class Rook(Piece):
         self.game = game
         super().__init__(self.game, pos, white, f"{'white' if white else 'black'} rook")
 
-    def move(self):
-        pass
-
     def get_valid_moves(self):
-        pass
+        moves = []
+        x, y = self.game.get_board_position()
+
+        # left
+        for i in range(x-1,-1,-1):
+            if self.game.pieces[y][i] == "":
+                moves.append((i, y))
+                continue
+
+            if self.game.pieces[y][i].is_white != self.is_white:
+                moves.append((i, y))
+            break
+
+        # right
+        for i in range(x+1, 8):
+            if self.game.pieces[y][i] == "":
+                moves.append((i, y))
+                continue
+
+            if self.game.pieces[y][i].is_white != self.is_white:
+                moves.append((i, y))
+            break
+
+        # up
+        for i in range(y-1,-1,-1):
+            if self.game.pieces[i][x] == "":
+                moves.append((x, i))
+                continue
+
+            if self.game.pieces[i][x].is_white != self.is_white:
+                moves.append((x, i))
+            break
+
+        # down
+        for i in range(y+1, 8):
+            if self.game.pieces[i][x] == "":
+                moves.append((x, i))
+                continue
+
+            if self.game.pieces[i][x].is_white != self.is_white:
+                moves.append((x, i))
+            break
+
+        return moves
